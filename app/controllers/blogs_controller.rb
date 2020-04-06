19c0724c167168c727e4de544d55fa209ad20306
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-06 18:34:59 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,15 +4,24 @@ class BlogsController < ApplicationController
 
   def show
   end
-
+#空のインスタンス変数をViewに渡す。
   def new
   	@blog = Blog.new
   end
-
+#
   def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
   end
 
 
   def edit
   end
+ # -----------------------------------
+ private
+
+ def blog_params
+ 	params.require(:blog).permit(:title, :category, :body)
+ end
 end
