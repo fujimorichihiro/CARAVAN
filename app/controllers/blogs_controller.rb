2e3b878567b4458751a7807c4467207d35efce4d
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-07 18:23:05 +0900

    [Start]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+  	@blog = Blog.find(params[:id])
   end
 #空のインスタンス変数をViewに渡す。
   def new
@@ -14,7 +15,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blogs_path(blog.id) #投稿後、詳細ページへ遷移
   end
 
 
